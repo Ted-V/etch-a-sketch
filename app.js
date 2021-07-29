a4a3d91492e32e97ec3d6cfd3df912e434e6e221
Ted Verdonkschot <tedverdonkschot@outlook.com>
2021-07-29 12:49:42 -0700

    Add functionality to clear button and resize slider

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Get grid container
+// Grab grid container
 let gridContainer = document.querySelector(".grid-container")
 
 // Add cells to grid 
@@ -11,11 +11,43 @@ function addCells(rows, cols) {
         cell.classList.add("grid-item");
         cell.addEventListener("mouseover", function colorCell(e) {
             e.target.style.background = "rgba(0, 0, 0, .9)";
-            e.target.style.border = "0px";
         });
-        cell.textContent = `${i}`;
         gridContainer.appendChild(cell);
     }
 }
 
+// Generate grid upon loading DOM
 document.body.onload = addCells(16, 16);
+
+// Track current grid dimensions
+let currentRows = 16;
+let currentCols = 16;
+
+// Grab clear button
+let clear = document.getElementById("clear-grid");
+
+// Delete all cells on click, then add uncolored cells back to the grid
+clear.addEventListener("click", function clearGrid() {
+    for (let i = 0; i < currentRows * currentCols; i++) {
+        gridContainer.removeChild(gridContainer.lastElementChild);
+    }
+    addCells(currentRows, currentCols);
+})
+
+// Clear the grid
+function clearGrid() {
+    for (let i = 0; i < currentRows * currentCols; i++) {
+        gridContainer.removeChild(gridContainer.lastElementChild);
+    }
+}
+
+// Grab slider
+let slider = document.getElementById("myRange");
+
+// Resize grid according to slider value
+slider.oninput = function resizeGrid() {
+    clearGrid();
+    currentRows = this.value;
+    currentCols = this.value;
+    addCells(currentRows, currentCols);
+}
